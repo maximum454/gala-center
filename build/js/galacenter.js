ab6a643f51054c324a1b52ee85f152c36f21658b
maxim <maximum454@gmail.com>
2021-04-29 18:13:58 +0600

    error delivery

--- build/js/galacenter.js
+++ build/js/galacenter.js
@@ -1086,14 +1086,15 @@ document.addEventListener('click', function (e) {
         }
 })
 document.addEventListener('click', function (e) {
-        const menuDrop = document.getElementById('nav-delivery');
-        const popup = document.querySelector('.nav-scroller__delivery-modal');
+        let parent = document.querySelector('.header-delivery');
+        let child = parent.querySelector('.header-delivery__modal');
+
         const target = e.target;
-        if (target.id != 'nav-delivery') {
-            popup.classList.remove("active");
+        if (target.classList != 'header-delivery') {
+            document.querySelector('.header-delivery__modal').classList.remove("active");
 
         } else {
-            popup.classList.toggle("active");
+            target.querySelector('.header-delivery__modal').classList.toggle("active");
         }
 })
 
