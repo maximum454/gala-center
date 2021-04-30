0a4761c3f8cf3f25e511413cadedb7e32f024a1d
maxim <maximum454@gmail.com>
2021-04-30 11:41:51 +0600

    merg

--- build/js/galacenter.js
+++ build/js/galacenter.js
@@ -1086,15 +1086,15 @@ document.addEventListener('click', function (e) {
         }
 })
 document.addEventListener('click', function (e) {
-        let parent = document.querySelector('.header-delivery');
-        let child = parent.querySelector('.header-delivery__modal');
+        let parent = document.getElementsByClassName('header-delivery');
+        let child = parent.getElementsByClassName('header-delivery__modal');
 
         const target = e.target;
         if (target.classList != 'header-delivery') {
-            document.querySelector('.header-delivery__modal').classList.remove("active");
+            child.classList.remove("active");
 
         } else {
-            target.querySelector('.header-delivery__modal').classList.toggle("active");
+            child.classList.toggle("active");
         }
 })
 
--- src/js/components/delivery-modal.js
+++ src/js/components/delivery-modal.js
@@ -1,12 +1,12 @@
 document.addEventListener('click', function (e) {
-        let parent = document.querySelector('.header-delivery');
-        let child = parent.querySelector('.header-delivery__modal');
+        let parent = document.getElementsByClassName('header-delivery');
+        let child = parent.getElementsByClassName('header-delivery__modal');
 
         const target = e.target;
         if (target.classList != 'header-delivery') {
-            document.querySelector('.header-delivery__modal').classList.remove("active");
+            child.classList.remove("active");
 
         } else {
-            target.querySelector('.header-delivery__modal').classList.toggle("active");
+            child.classList.toggle("active");
         }
 })
