8219738fae149b35a908b7853a622e959a41a142
maximum <454>
2021-05-06 22:26:37 +0600

    faq

--- src/js/components/accordeon.js
+++ src/js/components/accordeon.js
@@ -1,29 +1,11 @@
 $(function() {
-    $('.accordeon .accordeon__item').on('click', function (){
-        $('.accordeon .accordeon__body').not($(this).next()).slideUp(300);
-        $('.accordeon__body').next().slideToggle(300);
-    });
+    accordeon();
 });
 
 
 function accordeon(){
-    let accordeon = document.getElementsByClassName("accordion");
-    let i;
-
-    for (i = 0; i < accordeon.length; i++) {
-        accordeon[i].addEventListener("click", function() {
-            /* Переключение между добавлением и удалением класса "active",
-            чтобы выделить кнопку, управляющую панелью */
-            this.classList.toggle("active");
-
-            /* Переключение между скрытием и отображением активной панели */
-            var panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
-        });
-    }
+    $('.accordeon').on('click','.accordeon__item', function (){
+        $(this).toggleClass('active').siblings().removeClass('active');
+    })
 }
 
