7565390ae34b0560626fbcdd2b6ee4662a582298
maxim <maximum454@gmail.com>
2021-06-04 11:06:45 +0600

    mobile menu

--- src/js/components/menu-mobile.js
+++ src/js/components/menu-mobile.js
@@ -6,4 +6,16 @@ $(function(){
     $('.menu-mobile__close').on('click', function(){
         $('.menu-mobile').removeClass('show');
     })
+
+    $('.js-menu-mobile').on('click', function(){
+        $('.menu-mobile').addClass('active');
+        $('body').addClass('lock');
+    })
+
+    $('.menu-mobile__close').on('click', function(){
+        $('.menu-mobile').removeClass('active');
+        $('body').removeClass('lock');
+    })
+
+
 })
