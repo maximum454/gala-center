ddfd8d905ba951b40d0c028584e1e07b1ea8647d
maximum <454>
2021-06-22 22:42:31 +0600

    merg

--- src/js/components/slider-banners.js
+++ src/js/components/slider-banners.js
@@ -1,14 +1,48 @@
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    freeMode: true,
-    mousewheel: false,
-    initialSlide:0,
-    speed: 300,
-    navigation: {
-        nextEl: '.banners-next',
-        prevEl: '.banners-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-    },
-})
+var swiper = swiper;
+var swiperInit = false;
+
+
+function swiperMode() {
+    let mobile = window.matchMedia('(min-width: 0px) and (max-width: 1023px)');
+    let desktop = window.matchMedia('(min-width: 1024px)');
+
+    if (mobile.matches) {
+        if (swiper) {
+            swiper.destroy();
+            swiperInit = false;
+        }
+
+
+    }
+
+    else if (desktop.matches) {
+        if (!swiperInit) {
+            swiperInit = true;
+            swiper = new Swiper('.swiper-container', {
+                slidesPerView: 'auto',
+                freeMode: true,
+                mousewheel: false,
+                initialSlide: 0,
+                speed: 300,
+                navigation: {
+                    nextEl: '.banners-next',
+                    prevEl: '.banners-prev',
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+
+            });
+
+
+        }
+    }
+}
+
+window.addEventListener('load', function () {
+    swiperMode();
+});
+
+window.addEventListener('resize', function () {
+    swiperMode();
+});
