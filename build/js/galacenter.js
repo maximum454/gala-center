3d36ab2d68c95f4d7c2187ac6d3d824a07c03bf4
maxim <maximum454@gmail.com>
2021-06-24 12:57:28 +0600

    fix

--- build/js/galacenter.js
+++ build/js/galacenter.js
@@ -2239,11 +2239,15 @@ $(document).on('click', '.switch-mode', function (e) {
     if (target.hasClass('block')) {
         $('.catalog--list').removeClass('active').addClass('hide')
         $('.catalog--block').addClass('active').removeClass('hide')
-
+        
+        $('body').addClass('catalog-plitka');
+        $('body').removeClass('catalog-table');
     }
     if (target.hasClass('list')) {
         $('.catalog--list').addClass('active').removeClass('hide')
         $('.catalog--block').removeClass('active').addClass('hide')
+        $('body').addClass('catalog-table');
+        $('body').removeClass('catalog-plitka');
     }
 })
 
